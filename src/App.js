40f5ccd05d3898a81e673ccfb86259480a239af5
Donald Whalen <donaldwhalen@Donalds-MacBook-Pro.local>
2017-09-19 00:32:50 -0400

    Initial commit, TODO_ADD reducer and test

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to Redux
+          </h2>
+          <div className="TodoApp">
+            <TodoForm />
+            <TodoList todos={this.props.todos} />
+          </div>
+        </div>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
