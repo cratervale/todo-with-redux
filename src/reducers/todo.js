3d5c43d6138d7816ff7221610bb038e69515f6ca
Donald Whalen <donaldwhalen@Donalds-MacBook-Pro.local>
2017-09-21 03:12:25 -0400

    making components more classy, saving to server

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,4 +1,4 @@
-import {getTodos} from '../lib/todoServices'
+import {getTodos, createTodo} from '../lib/todoServices'
 
 const initState ={
   todos:[],
@@ -9,7 +9,9 @@ const CURRENT_UPDATE = 'CURRENT_UPDATE'
 const TODO_ADD = 'TODO_ADD'
 const TODOS_LOAD = 'TODOS_LOAD'
 
+// action creators
 export const updateCurrent = (val) => ({type:CURRENT_UPDATE, payload: val})
+export const addTodo = (todo) => ({type: TODO_ADD, payload: todo})
 export const loadTodos = (todos) =>({type: TODOS_LOAD, payload: todos})
 export const fetchTodos = () => {
   return (dispatch) => {
@@ -18,10 +20,17 @@ export const fetchTodos = () => {
   }
 }
 
+export const saveTodo = (name) => {
+  return (dispatch) => {
+    createTodo(name)
+    .then(res => dispatch(addTodo(res)))
+  }
+}
+
 export default (state = initState, action) => {
   switch(action.type){
     case TODO_ADD:
-      return {...state, todos: state.todos.concat(action.payload)}
+      return {...state, currentTodo: '', todos: state.todos.concat(action.payload)}
     case CURRENT_UPDATE:
       return {...state, currentTodo: action.payload}
     case TODOS_LOAD:
